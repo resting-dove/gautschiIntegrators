0fa8e711a7236fd805642fbeb796c6c4a21d197b
resting-dove <henri2@gmx.de>
2025-02-14 18:13:35 +0100

    remove velocity-verlet since this was not usable anymore

--- gautschiIntegrators/one_step.py
+++ gautschiIntegrators/one_step.py
@@ -4,53 +4,6 @@ from .base import Solver
 from .matrix_functions import MatrixFunctionEvaluator, WkmEvaluator
 
 
-class VelocityVerlet:
-    def __init__(self, h: float):
-        self.prev_force = None
-        self.h = h
-
-    def advance_positions(self, x: np.array, v: np.array, force: np.ndarray) -> np.array:
-        """Part one of the Velocity - Verlet scheme.
-
-        Parameters
-        ----------
-        x, v : array_like
-            Positions and velocities of the particles. Length is 3n.
-        force : array_like
-            Forces acting on the particles, already divided by the mass term. Length is 3n.
-        """
-        next_x = x + self.h * v + 0.5 * self.h ** 2 * force
-        self.prev_force = force
-        return next_x
-
-    def advance_velocities(self, v: np.array, force: np.ndarray) -> np.array:
-        """Part two of the Velocity - Verlet scheme.
-
-        Parameters
-        ----------
-        v : array_like
-            Positions and velocities of the particles. Length is 3n.
-        force : array_like
-            Forces acting on the particles, already divided by the mass term. Length is 3n.
-        """
-        next_v = v + 0.5 * self.h * (self.prev_force + force)
-        return next_v
-
-    def step(self, x: np.array, v: np.array, force: np.array) -> (np.array, np.array):
-        """Perform one step of the Velocity - Verlet scheme.
-
-        Parameters
-        ----------
-        x, v : array_like
-            Positions and velocities of the particles. Length is 3n.
-        force : array_like
-            Forces acting on the particles, already divided by the mass term. Length is 3n.
-        """
-        next_x = self.advance_positions(x, v, force)
-        next_v = self.advance_velocities(v, force)
-        return next_x, next_v
-
-
 class ExplicitEuler(Solver):
     """
     Solve second order differential equation $x'' = - \Omega^2 x + g(x)$ by transforming into first order ODE and
